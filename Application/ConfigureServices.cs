c8b483c7770b9fb821be163c9543243febac0c04
Ankush-032 <ankushdubey099@gmail.com>
2024-07-13 21:15:28 +0530

    Add project files.

---
+++ Application/ConfigureServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ConfigureServices 
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection serviceDescriptors)
+        {
+            serviceDescriptors.AddAutoMapper(Assembly.GetExecutingAssembly());
+            serviceDescriptors.AddMediatR(service =>
+            {
+                service.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                
+            });
+            return serviceDescriptors;
+        }
+    }
+}
---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection serviceDescriptors, 
+                                                                     IConfiguration configurtion)
+        {
+            return serviceDescriptors;
+        }
+    }
+}
